bb14b8a44f35431236174e3c7afd35a5e7898cdf
lyf-00 <yiff@mail.ustc.edu.cn>
2023-11-21 08:14:35 +0000

    update trained results

--- 1120.sh
+++ 1120.sh
@@ -23,6 +23,19 @@ WORLD_SIZE=6  CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 torchrun --nproc_per_node=6 --mas
   --val_set_size 0.2 \
   --adapter_name lora
 
+WORLD_SIZE=4 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 --master_port=3192 finetune.py   \
+    --base_model 'yahma/llama-7b-hf'   \
+    --data_path 'math_10k.json'  \
+    --output_dir './trained_models/llama-lora-10k/' \
+    --batch_size 16  \
+    --micro_batch_size 4   \
+    --num_epochs 3   \
+    --learning_rate 3e-4   \
+    --cutoff_len 256   \
+    --val_set_size 120 \
+    --eval_step 80 \
+    --save_step 80  \
+    --adapter_name lora
 
 # evaluation
 ## 1. for math_data.json finetuned model
@@ -126,6 +139,58 @@ CUDA_VISIBLE_DEVICES=2 python evaluate.py \
     --use_CoT \
     --save-suffix 'ft50k_CoT' 
 # END TODO
+
+## 3. for math_10k.json finetuned model
+CUDA_VISIBLE_DEVICES=0 python evaluate.py \
+    --model LLaMA-7B \
+    --adapter LoRA \
+    --dataset SVAMP \
+    --base_model 'yahma/llama-7b-hf' \
+    --lora_weights './trained_models/llama-lora-10k' \
+    --save-suffix 'ft10k'
+
+CUDA_VISIBLE_DEVICES=1 python evaluate.py \
+    --model LLaMA-7B \
+    --adapter LoRA \
+    --dataset MultiArith \
+    --base_model 'yahma/llama-7b-hf' \
+    --lora_weights './trained_models/llama-lora-10k' \
+    --save-suffix 'ft10k'
+
+CUDA_VISIBLE_DEVICES=2 python evaluate.py \
+    --model LLaMA-7B \
+    --adapter LoRA \
+    --dataset gsm8k \
+    --base_model 'yahma/llama-7b-hf' \
+    --lora_weights './trained_models/llama-lora-10k' \
+    --save-suffix 'ft10k'
+### COT
+CUDA_VISIBLE_DEVICES=3 python evaluate.py \
+    --model LLaMA-7B \
+    --adapter LoRA \
+    --dataset SVAMP \
+    --base_model 'yahma/llama-7b-hf' \
+    --lora_weights './trained_models/llama-lora-10k' \
+    --use_CoT \
+    --save-suffix 'ft10k_CoT' 
+CUDA_VISIBLE_DEVICES=4 python evaluate.py \
+    --model LLaMA-7B \
+    --adapter LoRA \
+    --dataset MultiArith \
+    --base_model 'yahma/llama-7b-hf' \
+    --lora_weights './trained_models/llama-lora-10k' \
+    --use_CoT \
+    --save-suffix 'ft10k_CoT' 
+
+CUDA_VISIBLE_DEVICES=5 python evaluate.py \
+    --model LLaMA-7B \
+    --adapter LoRA \
+    --dataset gsm8k \
+    --base_model 'yahma/llama-7b-hf' \
+    --lora_weights './trained_models/llama-lora-10k' \
+    --use_CoT \
+    --save-suffix 'ft10k_CoT' 
+    
 # without finetune
 # zero-shot task 不行，模型不懂格式，输出都是错的（不停重复）
 CUDA_VISIBLE_DEVICES=2 python evaluate.py \
